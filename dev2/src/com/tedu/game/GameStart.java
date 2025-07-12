7bb9b72fbfdd4e07a9ed06b1912e0c1a5686d44e
zhangyu <3230898906@qq.com>
2025-07-12 17:12:42 +0800

    提交manager包和game包

---
+++ dev2/src/com/tedu/game/GameStart.java
@@ -0,0 +1,13 @@
+package com.tedu.game;
+
+import com.tedu.show.DreamGardenFrame;
+
+public class GameStart {
+	/**
+	 * 程序的唯一入口
+	 */
+	public static void main(String[] args) {
+		DreamGardenFrame.setJPanel("MainJPanel");
+	}
+
+}
